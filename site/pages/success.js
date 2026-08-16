293aa8b3087fe17a9e34243686861ca56d96f848
qinisontombela4-boop <qinisontombela4@gmail.com>
2026-08-16 21:03:07 +0200

    Create success.js

---
+++ site/pages/success.js
@@ -0,0 +1,9 @@
+jsx
+// site/pages/success.js
+export default function Success() {
+  return (
+    <div className="bg-black text-gold min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl">Payment Successful!</h1>
+    </div>
+  );
+}
