36da2c153a931240b5a5f503ef502ac51ad78d3a
qinisontombela4-boop <qinisontombela4@gmail.com>
2026-08-16 21:03:52 +0200

    Create cancel.js

---
+++ site/pages/cancel.js
@@ -0,0 +1,9 @@
+jsx
+// site/pages/cancel.js
+export default function Cancel() {
+  return (
+    <div className="bg-black text-gold min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl">Payment Cancelled</h1>
+    </div>
+  );
+}
